548a90b7cfc787c5f23b38af2f120587a838f036
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-12-28 10:08:21 -0500

    adds some json functionality

--- public/js/leagues/player.factory.js
+++ public/js/leagues/player.factory.js
@@ -8,9 +8,12 @@
     PlayerFactoryFunction
   ]);
 
-  function PlayerFactoryFunction($resource){
-    return $resource("/jsonData/players.json", {}, {
-      query: {method:'GET'}
+  function PlayerFactoryFunction($resource,playerKey){
+    console.log(playerKey);
+    var url = "/jsonData/"+playerKey+".json";
+    console.log(url);
+    return $resource("/jsonData/:playerKey.json", {}, {
+      query: {method:'GET', params:{playerKey:'playerKey'}, isArray:true}
     });
   }
 }());
--- public/js/leagues/show.controller.js
+++ public/js/leagues/show.controller.js
@@ -11,16 +11,28 @@
   ]);
 
   function LeagueShowControllerFunction(LeagueFactory, PlayerFactory, $stateParams,scope){
+    var self = this;
     this.league = LeagueFactory.get({league_id: $stateParams.id});
+    this.playerList = [];
+    var playerArray = this.playerList
     this.league.$promise.then(function(data) {
+
       var length = data.query.results.league.draft_results.draft_result.length
+
       for (var i = 0; i < length; i++){
-        var player = data.query.results.league.draft_results.draft_result[i].player_key
+        var player = data.query.results.league.draft_results.draft_result[i];
+        var playerKey = data.query.results.league.draft_results.draft_result[i].player_key
+        var playerCost = data.query.results.league.draft_results.draft_result[i].cost
+        playerArray.push({'count':i,'id':playerKey,'cost':playerCost})
+        // console.log(playerArray);
+        var playerID = playerKey.slice(-4)
+        // console.log(playerKey);
+        // var playerInfo = [player.pick,player.round,player.cost]
+        self.players = PlayerFactory.get({playerKey: playerID}, function(playerData) {
+          playerArray.push({'name':playerData.query.results.player.name.full});
+        });
       }
-    });
-    this.players = PlayerFactory.query();
-    this.players.$promise.then(function(data) {
-      console.log(data.query);
+      console.log(playerArray);
     });
   }
 }());

07e490bc9b0a7d350be115f1060799108f4756ee
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-01-12 18:25:54 -0500

    reverts to horizontal chart

--- public/js/leagues/chart.directive.js
+++ public/js/leagues/chart.directive.js
@@ -51,7 +51,7 @@
         //in D3, any selection[0] contains the group
         //selection[0][0] is the DOM node
         //but we won't need that this time
-        // var chart = d3.select(element[0]);
+        var chart = d3.select(element[0]);
         //to our original directive markup bars-chart
         //we add a div with out chart stling and bind each
         //data entry to the chart
@@ -59,94 +59,95 @@
 
         scope.loadChart = function(){
 
-          // chart.append("div").attr("class", "chart")
-          // .selectAll('div')
-          // .data(scope.data).enter().append("div")
-          // .attr("class", "bar")
-          // .transition().ease("elastic")
-          // .style("width", function(d) { return d.cost + "%"; })
-          // .text(function(d) { return d.playerName +" $"+ d.cost; });
+          chart.append("div").attr("class", "chart")
+          .selectAll('div')
+          .data(scope.data).enter().append("div")
+          .attr("class", "bar")
+          .transition().ease("elastic")
+          .style("width", function(d) { return d.cost + "%"; })
+          .text(function(d) { return d.playerName +" $"+ d.cost; });
           //a little of magic: setting it's width based
           //on the data value (d)
           //and text all with a smooth transition
 
           // *******************************************************
-          var margin = {
-            top: 10,
-            right: 60,
-            bottom: 70,
-            left: 70
-          },w = 600,
-          h = 350;
-
-          var xScale = d3.scale.ordinal()
-          .domain(d3.range(scope.data.length))
-          .rangeRoundBands([0, w], 0.05);
-          // ternary operator to determine if global or cost has a larger scale
-          var yScale = d3.scale.linear()
-          .domain([0, d3.max(scope.data, function (d) {
-            return (d.cost);
-          })])
-          .range([h, 0]);
-          var xAxis = d3.svg.axis()
-          .scale(xScale)
-          .tickFormat(function (d) {
-            return scope.data[d].playerName;
-          })
-          var yAxis = d3.svg.axis()
-          .scale(yScale)
-          .orient("left")
-          .ticks(10);
-
-          //SVG element
-          var svg = d3.select(element[0])
-          .append("svg")
-          .attr("class", "chart")
-          .attr("width", w + margin.left + margin.right)
-          .attr("height", h + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          // Graph Bars
-          var sets = svg.selectAll(".set")
-          .data(scope.data)
-          .enter()
-          .append("g")
-          .attr("class", "set")
-          .attr("transform", function (d, i) {
-            return "translate(" + xScale(i) + ",0)";
-          });
-
-          sets.append("rect")
-          .attr("class", "cost")
-          .transition().ease("elastic")
-          .attr("width", xScale.rangeBand()/2)
-          .attr("y", function (d) {
-            return yScale(d.cost);
-          })
-          .attr("x", xScale.rangeBand()/6)
-          .attr("height", function (d) {
-            return h - yScale(d.cost);
-          })
-          ;
-
-          // xAxis
-          svg.append("g") // Add the X Axis
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0," + (h) + ")")
-          .call(xAxis)
-          .selectAll("text")
-          .attr("dx", "4em")
-          .attr("dy", "1em")
-          .style("text-anchor", "end")
-          .attr("transform", function (d) {
-            return "rotate(0)";
-          });
-          // yAxis
-          svg.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(0 ,0)")
-          .call(yAxis);
+          // var margin = {
+          //   top: 10,
+          //   right: 10,
+          //   bottom: 70,
+          //   left: 70
+          // },w = 600,
+          // h = 350;
+          //
+          // var xScale = d3.scale.ordinal()
+          // .domain(d3.range(scope.data.length))
+          // .rangeRoundBands([0, w], 0.05);
+          //
+          // var yScale = d3.scale.linear()
+          // .domain([0, d3.max(scope.data, function (d) {
+          //   return (d.cost);
+          // })])
+          // .range([h, 0]);
+          //
+          // var xAxis = d3.svg.axis()
+          // .scale(xScale)
+          // .tickFormat(function (d) {
+          //   return scope.data[d].playerName;
+          // })
+          //
+          // var yAxis = d3.svg.axis()
+          // .scale(yScale)
+          // .orient("left")
+          // .ticks(10);
+          //
+          // //SVG element
+          // var svg = d3.select(element[0])
+          // .append("svg")
+          // .attr("class", "chart")
+          // .attr("width", w + margin.left + margin.right)
+          // .attr("height", h + margin.top + margin.bottom)
+          // .append("g")
+          // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          //
+          // // Graph Bars
+          // var sets = svg.selectAll(".set")
+          // .data(scope.data)
+          // .enter()
+          // .append("g")
+          // .attr("class", "set")
+          // .attr("transform", function (d, i) {
+          //   return "translate(" + xScale(i) + ",0)";
+          // });
+          //
+          // sets.append("rect")
+          // .attr("class", "cost")
+          // .transition().ease("elastic")
+          // .attr("width", xScale.rangeBand())
+          // .attr("y", function (d) {
+          //   return yScale(d.cost);
+          // })
+          // .attr("x", xScale.rangeBand()/100)
+          // .attr("height", function (d) {
+          //   return h - yScale(d.cost);
+          // });
+          //
+          // // xAxis
+          // svg.append("g") // Add the X Axis
+          // .attr("class", "x-axis")
+          // .attr("transform", "translate(0," + (h) + ")")
+          // .call(xAxis)
+          // .selectAll("text")
+          // .attr("dx", "3em")
+          // .attr("dy", "1em")
+          // .style("text-anchor", "end")
+          // .attr("transform", function (d) {
+          //   return "rotate(0)";
+          // });
+          // // yAxis
+          // svg.append("g")
+          // .attr("class", "y-axis")
+          // .attr("transform", "translate(0 ,0)")
+          // .call(yAxis);
 
           // *******************************************************
 
